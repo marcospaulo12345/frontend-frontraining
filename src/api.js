8eb454f50ad0b455427174c1c9763929863b695d
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2022-10-04 12:52:47 -0300

    Aterado o ip do servidor

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://18.230.194.222:5000',
+    baseURL: 'http://18.230.194.222:5000',
     // baseURL: 'http://localhost:5000',
 
 })

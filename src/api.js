015438deff4cadfcd90dbf359b988a96feab2dc5
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-04-07 20:10:19 -0300

    Teste 2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://cba4-168-90-89-102.ngrok-free.app',
+    baseURL: 'https://2b49-177-66-117-187.ngrok-free.app',
     // baseURL: 'http://localhost:5000',
 
 })
